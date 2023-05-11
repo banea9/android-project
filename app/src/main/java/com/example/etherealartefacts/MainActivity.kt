d5a5db2c187e2462c9922a34e8a79c4d0ac63866
branislav.aleksov <branislav.aleksov@accedia.com>
2023-05-11 19:08:22 +0300

    remove unused import

--- app/src/main/java/com/example/etherealartefacts/MainActivity.kt
+++ app/src/main/java/com/example/etherealartefacts/MainActivity.kt
@@ -4,7 +4,6 @@ package com.example.etherealartefacts
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
